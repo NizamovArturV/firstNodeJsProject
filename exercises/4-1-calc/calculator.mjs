4335a29e91d11ccdeb672cd0af6729c546fa5e06
Artur Nizamov <a.nizamov@wptt.ru>
2023-02-24 16:11:22 +0500

    '4-2-calc-events'

--- exercises/4-1-calc/calculator.mjs
+++ exercises/4-1-calc/calculator.mjs
@@ -1,43 +1,61 @@
 import add from './calculationOperators/add.mjs';
 import subtract from './calculationOperators/subtract.mjs';
-import multiply from "./calculationOperators/multiply.mjs";
-import divide from "./calculationOperators/divide.mjs";
-import CalculatorError from "./calculatorError.mjs";
+import multiply from './calculationOperators/multiply.mjs';
+import divide from './calculationOperators/divide.mjs';
+import CalculatorError from './calculatorError.mjs';
 
 
 export default class Calculator {
 
     compute(firstNumber, secondNumber, operation) {
+
+        if (firstNumber === undefined || secondNumber === undefined || operation === undefined) {
+            throw new CalculatorError('Не указаны обязательные аргументы! ' +
+                'Введите аргументы по шаблону - node index.mjs #Первое число# #Второе число# #Оператор#');
+        }
+
         let result = 0;
+        operation = this.operationList.get(operation);
+
+        if (operation === undefined) {
+            throw new CalculatorError('Такого оператора не существует');
+        }
 
         firstNumber = Number(firstNumber);
         secondNumber = Number(secondNumber);
 
         if (isNaN(firstNumber) || isNaN(secondNumber)) {
-            throw new CalculatorError('Переданные параметры не являются числами')
+            throw new CalculatorError('Переданные параметры не являются числами');
         }
 
         switch (operation) {
             case 'add':
-            case '+':
                 result = add(firstNumber, secondNumber);
                 break;
             case 'subtract':
-            case '-':
                 result = subtract(firstNumber, secondNumber);
                 break;
             case 'multiply':
-            case '*':
                 result = multiply(firstNumber, secondNumber);
                 break;
             case 'divide':
-            case '/':
                 result = divide(firstNumber, secondNumber);
                 break;
-            default:
-                throw new CalculatorError('Такого оператора не существует');
         }
 
         return result;
+    }
+
+    get operationList() {
+        return new Map([
+            ['add', 'add'],
+            ['+', 'add'],
+            ['subtract', 'subtract'],
+            ['-', 'subtract'],
+            ['multiply', 'multiply'],
+            ['*', 'multiply'],
+            ['divide', 'divide'],
+            ['/', 'divide'],
+        ]);
     }
 }
---
+++ exercises/4-2-calc-events/app.mjs
@@ -0,0 +1,48 @@
+import Calculator from "../4-1-calc/calculator.mjs";
+import CalculatorError from "../4-1-calc/calculatorError.mjs";
+import EventEmitter from 'events';
+
+const eventEmitter = new EventEmitter();
+const calculator = new Calculator();
+
+const calculationInEvent = (firstNumber, secondNumber, operationCommand) => {
+    try {
+        eventEmitter.emit('result', calculator.compute(firstNumber, secondNumber, operationCommand));
+    } catch (e) {
+       eventEmitter.emit('error', e)
+    }
+}
+
+let firstNumber = process.argv[2];
+let secondNumber = process.argv[3];
+let operationCommand = process.argv[4];
+let operationListView = '';
+
+
+for (let operationName of calculator.operationList.keys()) {
+    eventEmitter.on(operationName, calculationInEvent);
+    operationListView += operationName + '\n';
+}
+
+eventEmitter.on('result', (result) => {
+    console.log('Результат вычислений:');
+    console.log(result);
+})
+
+
+eventEmitter.on('error', (e) => {
+    if (e instanceof CalculatorError) {
+        console.log('Ошибка вычислений:');
+        console.log(e.message);
+    } else {
+        throw e;
+    }
+})
+
+if (calculator.operationList.get(operationCommand) === undefined) {
+    console.log('Такой операции не существует! Укажите одну из команд:');
+    console.log(operationListView)
+} else {
+    eventEmitter.emit(operationCommand, firstNumber, secondNumber, operationCommand);
+}
+
